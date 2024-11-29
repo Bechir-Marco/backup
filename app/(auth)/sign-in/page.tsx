3f209581414c9184882c5f13cd2627d9f5520e68
Bechir-Marco <bechir.marko@gmail.com>
2024-11-29 23:39:41 +0100

    sign in and sign up layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,6 +1,5 @@
+import AuthForm from '@/components/AuthForm';
 import React from 'react';
 
-const SignIn = () => {
-  return <div>Page</div>;
-};
+const SignIn = () => <AuthForm type="sign-in"/>
 export default SignIn;
